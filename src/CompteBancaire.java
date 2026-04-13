deaf6400693f19cae24f7d9aaee6095a2e8a944f
Nills Maillet <mailletnills@gmail.com>
2026-04-13 14:28:29 +0200

    done: mission 2

---
+++ src/CompteBancaire.java
@@ -0,0 +1,27 @@
+public class CompteBancaire {
+    private String nom;
+    private float solde;
+
+    public CompteBancaire(String nom){
+        setNom(nom);
+        this.solde = 0;
+    }
+
+    public void setNom(String nom) {
+        if (nom != "") {
+            this.nom = nom;
+        }else {
+            System.out.println("Le nom ne peut être vide.");
+        }
+    }
+
+    public void ModifierSolde(float x){
+        if (x >= 0){
+            solde = x;
+            System.out.println("Le solde a bien été mis à jour.");
+        }else {
+            System.out.println("Le solde ne doit pas être négatif.");
+        }
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    CompteBancaire c = new CompteBancaire("");
 
+    // Ajout d'une méthode pour modifier le solde privé
+    c.ModifierSolde(-500);
 }
